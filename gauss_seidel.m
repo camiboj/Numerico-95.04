d656e45eff4c9a01a67612de51bfcc538d197df7
chortas <ceci.hortas@gmail.com>
2018-09-28 13:19:19 -0300

    gauss seidel listo y jacobi un poco modificado

--- gauss_seidel.m
+++ gauss_seidel.m
@@ -1,7 +1,9 @@
-function resultado = jacobi()
+function resultado = gauss_seidel()
   A = generador_matriz();
   b = generador_vector();
   n = length(b);
+  x=[];
+  x_iterate=[];
   error = 10^-5;
   
   x0 = [];  
@@ -9,20 +11,18 @@ function resultado = jacobi()
     x0(end+1) = 0; %semilla
   endfor
   x0 = transpose(x0);
-   
+  
   for j = 1 : n
-    suma_1 = A(j, [1:j-1]) * x0([1:j-1]);
+    %suma_1 = A(j, [1:j-1]) * x([1:j-1]);
     suma_2 = A(j, [j+1:n]) * x0([j+1:n]);
-    x(j) =  ((b(j) - suma_1 - suma_2) / A(j,j));  
+    x(j) =  ((b(j) -  A(j, [1:j-1]) * transpose(x([1:j-1])) - suma_2) / A(j,j));  
   endfor
   x1 = transpose(x);
   
-  k=1;
-  while (norm(x1-x0, inf) > error && k<1500)
+  while (norm(x1-x0, inf) > error && k<150)
     for j = 1 : n
-      suma_1 = A(j, [1:j-1]) * x1([1:j-1]);
       suma_2 = A(j, [j+1:n]) * x1([j+1:n]);
-      x_iterate(j) = ((b(j) - suma_1 - suma_2) / A(j,j));
+      x_iterate(j) = ((b(j) - A(j, [1:j-1]) * transpose(x_iterate([1:j-1])) - suma_2) / A(j,j));
     endfor
      x0 = x1;
      x1 = transpose(x_iterate);
@@ -30,7 +30,7 @@ function resultado = jacobi()
   endwhile
  
   if(k==150)
-    disp("Supero el numero de iteraciones permitido")
+    disp("Supero el numero de iteraciones permitido");
   endif
   resultado = x1;
 end  
