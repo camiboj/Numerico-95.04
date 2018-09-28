beb2a9af51a94f399705a562eee5298dd36b5e6d
chortas <ceci.hortas@gmail.com>
2018-09-28 20:00:15 -0300

    modificacion nombre archivos

--- calcular_error_porcentual.m
+++
@@ -1,9 +0,0 @@
-function error = calcular_error_porcentual_jacobi()
-  
-  A = generador_matriz();
-  b = generador_vector();
-  x_exacto = A\b;
-  [x_jacobi, k_jacobi] = jacobi(A,b);
-  
-  error = norm(xexacto-xjacobi) / norm(xexacto)
-end
