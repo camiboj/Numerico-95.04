3bd6342a92a55c5751049cb402a9d69713a78960
chortas <ceci.hortas@gmail.com>
2018-09-28 16:34:11 -0300

    estilo

--- gauss_seidel.m
+++ gauss_seidel.m
@@ -1,24 +1,21 @@
-function resultado = gauss_seidel()
-  A = generador_matriz();
-  b = generador_vector();
+function resultado = gauss_seidel(A,b)
   n = length(b);
-  x=[];
-  x_iterate=[];
   error = 10^-5;
   
   x0 = [];  
   for i=1: 81
     x0(end+1) = 0; %semilla
   endfor
-  x0 = transpose(x0);
-  
+  x0 = x0';
+
+  x=[];  
   for j = 1 : n
-    %suma_1 = A(j, [1:j-1]) * x([1:j-1]);
     suma_2 = A(j, [j+1:n]) * x0([j+1:n]);
-    x(j) =  ((b(j) -  A(j, [1:j-1]) * transpose(x([1:j-1])) - suma_2) / A(j,j));  
+    x(j) =  ((b(j) -  A(j, [1:j-1]) * (x([1:j-1]))' - suma_2) / A(j,j));  
   endfor
-  x1 = transpose(x);
-  
+  x1 = x';
+
+  x_iterate=[];  
   while (norm(x1-x0, inf) > error && k<150)
     for j = 1 : n
       suma_2 = A(j, [j+1:n]) * x1([j+1:n]);
@@ -28,9 +25,10 @@ function resultado = gauss_seidel()
      x1 = transpose(x_iterate);
      k = k + 1;
   endwhile
- 
+  
   if(k==150)
     disp("Supero el numero de iteraciones permitido");
   endif
+  
   resultado = x1;
 end  
--- jacobi.m
+++ jacobi.m
@@ -1,6 +1,4 @@
-function resultado = jacobi()
-  A = generador_matriz();
-  b = generador_vector();
+function resultado = jacobi(A,b)
   n = length(b);
   error = 10^-5;
   
@@ -8,29 +6,30 @@ function resultado = jacobi()
   for i=1: 81
     x0(end+1) = 0; %semilla
   endfor
-  x0 = transpose(x0);
+  x0 = x0';
    
   for j = 1 : n
     suma_1 = A(j, [1:j-1]) * x0([1:j-1]);
     suma_2 = A(j, [j+1:n]) * x0([j+1:n]);
     x(j) =  ((b(j) - suma_1 - suma_2) / A(j,j));  
   endfor
-  x1 = transpose(x);
+  x1 = x';
   
   k=1;
-  while (norm(x1-x0, inf) > error && k<1500)
+  while (norm(x1-x0, inf) > error && k<150)
     for j = 1 : n
       suma_1 = A(j, [1:j-1]) * x1([1:j-1]);
       suma_2 = A(j, [j+1:n]) * x1([j+1:n]);
       x_iterate(j) = ((b(j) - suma_1 - suma_2) / A(j,j));
     endfor
      x0 = x1;
-     x1 = transpose(x_iterate);
+     x1 = x_iterate';
      k = k + 1;
   endwhile
  
   if(k==150)
     disp("Supero el numero de iteraciones permitido")
   endif
+  
   resultado = x1;
 end  
