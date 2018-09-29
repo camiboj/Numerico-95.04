94df4b63ce14c0ac5096e4c93611d6914599bf6b
camiboj <camiboj@gmail.com>
2018-09-29 19:01:32 -0300

    ahora también grafica

--- tp.m
+++ tp.m
@@ -2,7 +2,10 @@ function tp
   A = generador_matriz();
   b = generador_vector();
   x_directo = A\b;
+  plotear(x_directo, "Directo");
   [x_jacobi, k_jacobi] = jacobi(A,b);
+  plotear(x_jacobi, "Jacobi");
   [x_gauss_seidel, k_gauss_seidel] = gauss_seidel(A,b);
+  plotear(x_gauss_seidel, "Gauss Saidel");
 
 end  
