bc6ae059a0e5088bc69d255ce0554c74f0c5a4ac
Cecilia Hortas <ceci.hortas@gmail.com>
2018-11-23 17:51:29 -0300

    graficar no funciona como queremos que funcione pero lo pusheo para tener una nocion

---
+++ TP2/graficar.m
@@ -0,0 +1,24 @@
+function graficar(T, U_euler, U_lsode, U_rk2, U_rk4)
+
+  hold on;
+  
+  title("Escenario 1");
+  xlabel("Trayectoria");
+  ylabel("Tiempo");
+  
+  plot(T, U_euler(:, 1), 'k-.',
+       T, U_lsode(:, 1), 'm*', 
+       T, U_rk2(:, 1), 'g--', 
+       T, U_rk4(:, 1), 'b-');
+       
+  plot(T, U_euler(:, 3), 'k-.',
+       T, U_lsode(:, 3), 'm*', 
+       T, U_rk2(:, 3), 'g--', 
+       T, U_rk4(:, 3), 'b-');
+  
+  h = legend('Euler', 'Lsode', 'Runge-Kutta Orden 2', 'Runge-Kutta Orden 4');
+  legend(h, 'location', 'southwest');
+  
+  hold off;
+  
+endfunction
