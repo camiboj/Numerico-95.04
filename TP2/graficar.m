41e2a7e69176c23f77c79a0173981f112336befc
Cecilia Hortas <ceci.hortas@gmail.com>
2018-11-28 12:27:34 -0300

    soluciono problemas de merge

--- TP2/graficar.m
+++ TP2/graficar.m
@@ -1,29 +1,4 @@
-<<<<<<< HEAD
-function graficar(T, U_euler, U_lsode, U_rk2, U_rk4)
-
-  hold on;
-  
-  title("Escenario 1");
-  xlabel("Trayectoria");
-  ylabel("Tiempo");
-  
-  plot(T, , 'k-.',
-       T, U_lsode(:, 1), 'm*', 
-       T, U_rk2(:, 1), 'g--', 
-       T, U_rk4(:, 1), 'b-');
-       
-  plot(T, U_euler(:, 3), 'k-.',
-       T, U_lsode(:, 3), 'm*', 
-       T, U_rk2(:, 3), 'g--', 
-       T, U_rk4(:, 3), 'b-');
-  
-  h = legend('Euler', 'Lsode', 'Runge-Kutta Orden 2', 'Runge-Kutta Orden 4');
-  legend(h, 'location', 'southwest');
-  
-  hold off;
-=======
 function graficar(T, U_euler, U_lsode, U_rk2, U_rk4, titulo, num)
->>>>>>> b2a7a2d8c330b3c204a7f1fa26bf2dcfec779ace
   
 
   _graficar(T, U_euler, U_lsode, U_rk2, U_rk4, titulo, "Susceptibles", 1, num+1);
